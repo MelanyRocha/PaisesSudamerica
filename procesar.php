62c00f32bfb330f8dc2584df7e19730e26ba8cdb
MelanyRocha <melanyrochaferrufino@gmail.com>
2023-10-10 20:53:41 -0400

    cambio id

--- procesar.php
+++ procesar.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de la base de datos
-$servername = "localhost"; // Cambia a la dirección del servidor de tu base de datos
-$username = "root"; // Cambia al nombre de usuario de tu base de datos
-$password = ""; // Cambia a la contraseña de tu base de datos
-$dbname = "paisessudamericanos"; // Cambia al nombre de tu base de datos
+$servername = "db4free.net:3306"; // Cambia a la dirección del servidor de tu base de datos
+$username = "sistemasinf"; // Cambia al nombre de usuario de tu base de datos
+$password = "sistemasinf"; // Cambia a la contraseña de tu base de datos
+$dbname = "sisinfpaises"; // Cambia al nombre de tu base de datos
 
 // Crear una conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $extension = $_POST["extension"];
 
     // Realizar la inserción en la base de datos
-    $sql = "INSERT INTO pais (nombre, extencion) VALUES ('$nombre', '$extension')";
+    $sql = "INSERT INTO pais (NombrePais, ExtensionPais) VALUES ('$nombre', '$extension')";
 
     if ($conn->query($sql) === TRUE) {
         // Inserción exitosa
